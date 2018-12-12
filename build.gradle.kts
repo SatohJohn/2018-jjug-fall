4eeaaacb5b0379aebc80a8363976cdf44bcf44ec
john <sato.keita@furyu.jp>
2018-12-12 18:41:56 +0900

    gradleのライブラリを修正したらどうにかしたり

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
 plugins {
     kotlin("jvm") version "1.3.10"
-    java
-    idea
 }
--- kotlin-sample/src/main/kotlin/examples/KotlinScope.kt
+++ kotlin-sample/src/main/kotlin/examples/KotlinScope.kt
@@ -3,11 +3,9 @@ package examples
 private class TestObject(val param: String)
 
 fun main(args: Array<String>) {
-    val testObject: TestObject = "parameter".also { param ->
-        println(param)
-    }.let { param ->
-        create(param.toUpperCase())
-    }
+    val testObject: TestObject = "parameter"
+            .also { println(it) }
+            .let { create(it.toUpperCase()) }
 }
 
 private fun create(param: String) = TestObject(param)
