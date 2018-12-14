52f3ab7768d5e982adb81cf7510e515051b385a1
john <sato.keita@furyu.jp>
2018-12-14 19:38:12 +0900

    コメントを追加した

--- kotlin-sample/src/main/kotlin/examples/KotlinDataClass.kt
+++ kotlin-sample/src/main/kotlin/examples/KotlinDataClass.kt
@@ -7,10 +7,13 @@ data class User(
 )
 
 fun main(args: Array<String>) {
+    // 分割で受け取ることが可能
 //    var (uid, name) = User("uid", "name", "mailAddress")
 //    println("${uid}, ${name}")
+    // _を使うとその部分は使わないということが可能
 //    var (_, name) = User("uid", "name", "mailAddress")
 //    println("${name}")
+    // 順番が大事で name には"uid"が入る
     var (name) = User("uid", "name", "mailAddress")
     println("${name}")
 }
